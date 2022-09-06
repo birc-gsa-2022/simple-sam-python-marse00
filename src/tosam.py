7ec14215b5a631cdcbac6b3067c807735dfcd01b
Goncalo Marcelino <gmmarcelino8@gmail.com>
2022-09-06 12:04:51 +0200

    Completed exercise

--- src/tosam.py
+++ src/tosam.py
@@ -8,10 +8,9 @@ def main():
         type=argparse.FileType('r')
     )
     args = argparser.parse_args()
-    print(args)
     for line in args.mas:
         chrom, read_name, read_str, pos = line.split('\t')
-        # Output as Simple-SAM
+        print(f'{read_name.strip()}\t{chrom.strip()}\t{int(pos.strip())+1}\t{len(read_str)}M\t{read_str.strip()}')
 
 
 if __name__ == '__main__':
